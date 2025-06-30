62c0f54b4002b290ce8ebdc73fff14bf52a3ffcd
hieuntkma <hieuntkma@gmail.com>
2025-06-30 14:29:59 +0700

    fix

--- store/urls.py
+++ store/urls.py
@@ -33,6 +33,8 @@ urlpatterns = [
     path('', include('portal.urls', namespace='portal')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 for app, urls in APP_LIST.items():
     urlpatterns.append(path(urls, include(f'{app}.urls')))
