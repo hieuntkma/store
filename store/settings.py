f52a50d912c0183c7012672d799764a741099bb4
hieuntkma <hieuntkma@gmail.com>
2025-06-30 22:34:55 +0700

    merge hieunt

--- store/settings.py
+++ store/settings.py
@@ -174,8 +174,8 @@ DATABASES = {
         'NAME': NS_store_DB_NAME,
         'USER': NS_store_DB_USER,
         'PASSWORD': NS_store_DB_PASSWORD,
-        # 'HOST': 'db',
-        'HOST': NS_store_DB_HOST,
+        'HOST': 'db',
+        # 'HOST': NS_store_DB_HOST,
         'PORT': NS_store_DB_PORT,
         'TEST': {
             'NAME': NS_store_DB_TEST_NAME,
